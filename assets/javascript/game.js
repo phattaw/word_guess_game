d8d48d2046b8d29521ee8fb7bc31bf810e273101
Phattaw <phattaw@gmail.com>
2019-03-10 20:12:02 -0500

    finished hang person game

---
+++ assets/javascript/game.js
@@ -0,0 +1,180 @@
+
+
+var random_words_array = [ "Withstander", "undeepened", "asexualize", "coinsure", "untraceried", "fillibeg", "fogbow", "hydrolyser", "joannes", "patd", "disingenuously", "vern", "expos", "manta", "Maplelike", "styria", "unpriced", "lothar", "litchfield", "gomeril", "unphonnetical", "glop", "retrieval", "flannelly", "umayyad", "expansionistic", "milldam", "filler", "Sikhara", "auditory", "paraphernalia", "forego", "deduct", "immigratory", "georgina", "dealings", "brickyard", "confiner", "mankiewicz", "conspicuous", "misspell", "papyral", "Hexafluoride", "foodlessness", "nonchallenging", "dato", "haphtarah", "slutty", "chequebook", "agouty", "uniaxially", "conception", "redissolving", "swallet", "jacksnipe", "archiepiscopality", "Restipulate", "audrey", "gonion", "fibroin", "uvulitis", "explanatively", "hoist", "antimensia", "outfling", "unpresiding", "upsurge", "repurifying", "circumscribed", "reincrease",
+    "Leiden", "ironbark", "theologue", "cryptogam", "trothless", "effeminate", "unsummable", "uranometry", "unpublicized", "haversine", "libational", "guadalquivir", "emitting", "abrogative", "Have", "martellando", "sigsbee", "armomancy", "misnavigating", "lah", "misfire", "amphoterus", "unslouchy", "subcalibre", "sabbath", "fritz", "uke", "rhodos", "Displaced", "condense", "i''s", "androgynous", "virtuality", "fitzgerald", "overstimulating", "nonabsolutistic", "satb", "canceled", "traveling", "gallfly", "uncleavable", "Anhedonic", "anim", "uncountenanced", "repurposing", "subpattern", "card", "hypogeum", "preillustration", "presufficiency", "palladized", "kulturkampf", "sain", "sargo", "tensionless", "Defencelessness", "ichnite", "unsatirised", "unsubdivided", "uncustomary", "penmanship", "sunback", "cockcrow", "packaged", "calcine", "ascription", "clavus", "bergamee", "costless", "dilative", "motorboating",
+    "Expressible", "repursued", "blackhead", "cystostomy", "meagrely", "reanalyzing", "backstroke", "videvdat", "parton", "eduard", "bigwig", "organzine", "nervy", "giordano", "Manitu", "dvorak", "sought", "clingfishes", "repay", "periodization", "diffractively", "distrainor", "lexicography", "nonocclusive", "prebetrothal", "saurian", "stood", "hypermetric", "Skydive", "undialyzed", "hypermegasoma", "underconcerned", "going", "nonnarcissistic", "pleasurableness", "prestimulate", "toelike", "kanazawa", "hardily", "kendal", "monolithism", "aeolipile", "Retort", "macrodome", "stoneroller", "razee", "unpropitiating", "cabbage", "gel", "frugality", "impeded", "vociferance", "jahwe", "missilry", "anglesite", "nonexpressionistic", "Jesmine", "unameliorable", "cuttlefish", "macrogamete", "pimp", "curtius", "euphemize", "partaker", "didymous", "eskishehir", "spendthrift", "portulaca", "unreverberated", "freelancer",
+    "Unsoothed", "microlambert", "devolvement", "auscultative", "bremsstrahlung", "goby", "december", "grandparent", "nanak", "bilinear", "spoof", "nonconservative", "stylohyoid", "wae", "Acrylaldehyde", "richter", "minipill", "undiscovered", "cantoris", "discretion", "unstatable", "supersensualistic", "obligor", "pedestrianism", "corroborated", "der", "suspender", "unlaconic", "Cryptograph", "momentarily", "bezzant", "constrainable", "ambrettolide", "nonsubsidiaries", "vintaged", "prereligious", "fornicatory", "microbalance", "agglutinating", "interdifferentiation", "synergist", "tunguses", "Overchill", "projectionist", "untestamentary", "unappointed", "superadaptable", "antidotically", "satellitic", "unfailing", "may", "quino", "unrecited", "nonchalance", "demureness", "geographic", "Interlaminar", "hexosan", "rapacki", "condyle", "resistor", "lyssophobia", "semiductile", "pilotage", "picturesquely", "glottalize", "scour", 
+    "Earing", "praetorianism", "equiponderated", "fineries", "overworking", "premeassuring", "regretably", "cromorne", "noninfusible", "telephonic", "unrigid", "ungalled", "dolorously", "annularity", "Topeka", "pantropic", "facile", "nay", "sheeny", "osmometry", "renownedness", "distilland", "obligee", "polynuclear", "interstitial", "liquefactive", "hyraces", "eirenicon", "Papillar", "razorback", "beeb", "pivotally", "cajolingly", "coraji", "equiprobably", "stigmasterol", "westlings", "unsieved", "variole", "proglottis", "penstemon", "gasconaded", "Ultracentrifugal", "drinkableness", "inklike", "cockfight", "summariness", "editress", "seaplane", "bolvar", "overclogged", "nondisciplinary", "acetophenetidin", "tombola", "inventable", "uncapitalised", "Otherwhiles", "larithmic", "oink", "nixie", "spoony", "effulge", "unworshiping", "conformably", "hereabout", "queening", "salverform", "exsertile", "unirrigable", "mediastinal",
+    "Woolworth", "affectionate", "instigator", "divulged", "renullify", "briskly", "antiblastic", "homeothermal", "hordeolum", "pulverised", "polymnestor", "enthymeme", "unfulminating", "tritonis", "Necropolises", "resmooth", "dendritically", "spinnable", "dialysate", "boarishness", "guestimating", "gingelly", "infectiveness", "assassin", "stalemated", "cerebellums", "crystal", "slummy", "Confection", "committeeship", "bhikshu", "exodus", "semipagan", "joint", "giuseppe", "filate", "birl", "goosey", "overheady", "retallying", "chinaware", "Preorder", "rouble", "fodder", "ophytic", "biographically", "embroidery", "forever", "reselection", "didicoy", "frï¿¥ï¾½chet", "unpercipient", "niv", "disputed", "twittery", "phil", "undispersed", "tartar", "faraday", "outrigged", "mechanismic", "unbesmeared", "caner", "confusably", "overnumerous", "skyscraping", "hyperspatial", "pulverising", "ajaccio", "flail", "supertreason",
+    "Rodenticide", "dimerized", "trophy", "checkweigher", "bilboes", "cotopaxi", "caducei", "uncommonly", "aleixandre", "pushingness", "motiveless", "lacework", "semineurotically", "reabsorption", "Bottegas", "intendedly", "accordature", "senescence", "interpenetrate", "musicological", "laniary", "cognominal", "reblend", "unperished", "interlocated", "anticoagulative", "reengraving", "ecsc", "Blaspheme", "vestibuled", "charlatan", "misoccupying", "filial", "girtin", "elvish", "coterie", "teratogeny", "tycho", "hypacusia", "uncitizenly", "preexperienced", "gauntness", "Interfiling", "unpredicative", "pollock", "sambaing", "intercalated", "naumann", "esterifiable", "amethyst", "zabrze", "adipocerite", "incessantness", "unmodernized", "laceration", "superpatience", "Mtb", "vivifying", "repunctuation", "whomever", "girlie", "supertempt", "wipe", "proconsultation", "vaccinationist", "leucocythemia", "unsurrendered", "velure", "stretcher", 
+    "Supersensibly", "phrixus", "porchlike", "semiacidic", "developmental", "unaccidental", "malnutrition", "thermogalvanometer", "convener", "resecure", "dissyllable", "paramountly", "continence", "radioautograph", "Harl", "presatisfy", "aerographic", "nonreplacement", "populationless", "tiring", "polyembryony", "erasure", "armagnac", "ens", "nonprohibitive", "undersequence", "fleshy", "aznavour", "Boulangism", "reglorifying", "postneuralgic", "subcomplete", "lamppost", "frauen", "cacophony", "attentive", "precapture", "mayorship", "ausforming", "epiphenomenally", "aluminographic", "myelographically", "Enzymolytic", "panegyrized", "choreic", "luwian", "accidental", "chlortetracycline", "nondecoration", "sotol", "trammeling", "rawalpindi", "latinus", "belief", "normative", "awry", "Nonegotistic", "unconnotative", "tarai", "preindebted", "seleucid", "facetiae", "cageling", "bookmarker", "ridgeway", "aqaba", "wallaby", "evectional", 
+    "Noninformational", "reglazed", "styr", "organotherapy", "droughty", "anthraces", "pleadingness", "unveering", "overpromise", "corporate", "fomentation", "abdominous", "verrazano", "beguine", "Omophagia", "macerator", "bukharin", "fleshhook", "glided", "vomiturition", "unconversational", "glaber", "fangled", "paloverde", "spital", "uniserial", "palinode", "heterochromosome", "Anoopsia", "insociably", "dictational", "litchi", "measurement", "endamoeba", "nostril", "set", "semimatte", "commemorable", "sesame", "ridden", "seminole", "episepalous", "Bokharan", "nematological", "elopement", "hypothesis", "limnetic", "auriferous", "vapor", "wincing", "uncanned", "tuberous", "intertransformability", "multicharge", "multicide", "extant", "Viperously", "eth", "annunciate", "phytohormone", "symbolise", "jizyah", "oblique", "narberth", "stomata", "intermet", "presubsistent", "yttrotantalite", "preindemnify", "turbid", "perimorphic",
+    "Traducement", "providence", "dodgem", "umbellately", "satisfyingness", "showroom", "unthrone", "lockport", "chayefsky", "supervigilance", "nonenigmatic", "nanhai", "unresearched", "grayish", "Rayon", "unorganizable", "saponaceousness", "hordein", "schizogamy", "compliance", "vesting", "moina", "magenta", "welshman", "grittiest", "southeasterner", "sacrum", "clinton", "Noncondensibility", "reexploring", "overwealth", "melvin", "invent", "laelaps", "transposability", "reconverge", "astrologically", "principe", "yankee", "saros", "riksmaal", "snugger", "Escritoire", "pseudohemophilia", "unsolved", "coupling", "recommendatory", "transelementating", "nonprobability", "roundhead", "saltatorial", "prelabial", "denton", "mohammedanise", "hectare", "lightsomely", "Peasant", "telltalely", "unstaggering", "lindbergh", "afforestment", "preconscious", "foggily", "proritual", "convoluted", "isopag", "saltpetre", "bandelet", "chumash", "felspathose" ];
+
+
+$(document).ready(function() {
+
+    var hangman_word_idx;
+    var guessed_characters;
+    var hangman_chars;
+    var guessesMade;
+    var guessesLeft;
+    var current_word;
+    var curr_word_array;
+    var wins;
+
+    resetGame();
+    // 5. Press any key to get started!
+    $(".help_text").text("Press any key to get started! Type characters between a-z!");            
+    render();
+
+    // 3. Use key events to listen for the letters that your players will type.
+    $(document).keyup( function(e) {
+        var char_pressed = String.fromCharCode(e.which).toLowerCase();
+
+        // 9. After the user wins/loses the game should automatically choose another word and make the user play it.
+        if(guessesLeft === guessesMade) {
+            resetGame();            
+            $(".help_text").text("Methinks thou dost need more practice! Have at this word!");            
+        }
+        else {
+            if( char_pressed >= 'a' && char_pressed <= 'z') {
+                for(let i = 0; i < hangman_chars.length; i++ ) {
+                    if(String.fromCharCode(e.which).toLowerCase() === hangman_chars[i].cur_char.toLowerCase() ) {
+                    hangman_chars[i].found = true;
+                    }
+                }
+
+                // if(guessed_characters.length === 0) {
+                //     guessed_characters.push(char_pressed)
+                // }
+
+                var found = guessed_characters.find( function(character) {
+                    return( character === char_pressed);
+                });
+    
+                var cur_found = curr_word_array.find( function(character) {
+                    return( character === char_pressed);
+                    });
+    
+                if(found === undefined) {
+                    guessed_characters.push(char_pressed);
+    
+                    if(typeof(cur_found) === "string") {
+                        if(checkWordGuessed() == true) {
+                        resetGame();            
+                        $(".help_text").text("Dilly Dilly! Play again!");
+                        wins++;
+                        }
+                        else {
+                        $(".help_text").text("Passable try! Prithee thee guess again!");            
+                        }
+                    }
+                    else {
+                        guessesMade++;
+                        if( guessesLeft === guessesMade) {
+                        resetGame();            
+                        $(".help_text").text("Methinks thou dost need more practice! Have at this word!");            
+                        }
+                        else {
+                        $(".help_text").text("Methinks literacy is not your strong point! Guess again peasant!");
+                        }
+                    }
+                }
+                else {
+                $(".help_text").text("The lords english has more letters than that!");
+                }
+            }
+            else {
+            $(".help_text").text("Thou art a scallywag! Type characters between a-z!");            
+            }    
+        }
+
+        render();
+    } );
+
+    function render() {
+        var winsDiv = $("<p>");
+        var guessDiv = $("<p>");
+
+        updateWordToGuess();
+
+        // 6. Wins: (# of times user guessed the word correctly).
+        winsDiv.text("Wins: " + wins);
+        $("#wins").empty();
+        $("#wins").append(winsDiv);
+
+        // 7. Number of Guesses Remaining: (# of guesses remaining for the user).
+        $("#guesses_left").empty();
+        $("#guesses_left").append(guessesLeft - guessesMade);
+
+        $("#guesses").empty();
+        $("#guesses").append(guessDiv);
+
+        // 8. Letters Already Guessed: (Letters the user has guessed, displayed like `L Z Y H`).
+        var guesses = "";
+        for(var i = 0; i < guessed_characters.length; i++) {
+            guesses += guessed_characters[i] + " ";
+        }
+        
+        guessDiv.text(guesses);    
+    }
+
+    function updateWordToGuess () {
+        var wordDiv = $("<p>");
+        var word_to_guess = "";
+
+        for(let i = 0; i < hangman_chars.length; i++ ) {
+            if(hangman_chars[i].found === true || guessesMade === guessesLeft)
+            {
+            // As the user guesses the correct letters, reveal them: `m a d o _  _ a`.
+            word_to_guess += hangman_chars[i].cur_char + " ";
+            }
+            else {
+            // If the word is `madonna`, display it like this when the game starts: `_ _ _ _ _ _ _`.
+            word_to_guess += "_ ";
+            }
+        }
+
+        wordDiv.text(word_to_guess);
+        $("#random-word").empty();
+        $("#random-word").append(wordDiv);
+        }
+
+    function checkWordGuessed () {
+        for(var i=0; i < hangman_chars.length; i++) {
+            if( hangman_chars[i].found === false ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function resetGame () {
+        hangman_word_idx = Math.floor((Math.random() * random_words_array.length));
+        guessed_characters = [];
+        hangman_chars = [];
+        guessesMade = 0;
+        guessesLeft = 10;
+        wins = 0;
+
+        if(current_word !== undefined) {
+            $("#last_word").text(current_word);
+        }
+
+        current_word = random_words_array[hangman_word_idx];
+        curr_word_array = current_word.split("");
+    
+        for(var i=0; i < curr_word_array.length; i++) {
+            var hangman_obj = {
+                cur_char : '',
+                found : false
+            }
+    
+            hangman_obj.cur_char = curr_word_array[i];
+            hangman_chars.push(hangman_obj);
+        }
+    }
+});
+
